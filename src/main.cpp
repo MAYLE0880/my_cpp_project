08b2a31e7643177969d0d1b6f86aeac2c6620810
Mayle0880 <k.gaylevich@inbox.ru>
2026-09-12 18:17:02 +0300

    commit to my project

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+using namespace std;
+
 int main() {
-std::cout << "Updated message!" << std::endl;
-return 0;
+    float f = 9.99f;
+    int i = (int)f;
+    char c = (char)i;
+
+    cout << "float f = " << f << endl;
+    cout << "int i = " << i << endl;
+    cout << "char c = " << (int)c << endl;
 }
